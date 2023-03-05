85f5b5477222e898061f29da25dab9750a36f9e5
Syedakram-code <81096905+Syedakram-code@users.noreply.github.com>
2023-03-05 12:14:00 +0530

    ...

--- src/app/Portfolio/components/profile/profile.component.ts
+++ src/app/Portfolio/components/profile/profile.component.ts
@@ -9,9 +9,9 @@ export class ProfileComponent {
   MyName:string = "Syed Akram Mehdi";
   @Input() dayMode!: boolean;
  socialMediaLinks = [
-  {toolTip : 'LinkedIn' , src : '/assets/images/linkedin.png' , link:'https://www.linkedin.com/in/syed-akram-mehdi-7b42b4183'},
-  {toolTip : 'GitHub' , src : '/assets/images/github-sign.png',link:'https://github.com/Syedakram-code'},
-  {toolTip : 'Instagram' , src : '/assets/images/instagram.png',link:'https://www.instagram.com/_____syed_____akram_____/'},
-  {toolTip : 'WhatsApp' , src : '/assets/images/whatsapp.png',link:'https://wamsg.me/l6zl'},
+  {toolTip : 'LinkedIn' , src : './assets/images/linkedin.png' , link:'https://www.linkedin.com/in/syed-akram-mehdi-7b42b4183'},
+  {toolTip : 'GitHub' , src : './assets/images/github-sign.png',link:'https://github.com/Syedakram-code'},
+  {toolTip : 'Instagram' , src : './assets/images/instagram.png',link:'https://www.instagram.com/_____syed_____akram_____/'},
+  {toolTip : 'WhatsApp' , src : './assets/images/whatsapp.png',link:'https://wamsg.me/l6zl'},
  ]
 }
