3989f613c7bf899764c0b85713c1634ce3794d13
developer <Developer@mail.com>
2023-05-31 23:50:59 +0530

    Added Acadamics's

--- src/app/Portfolio/components/Cards/profile-section-components/acadimics/acadimics.component.ts
+++ src/app/Portfolio/components/Cards/profile-section-components/acadimics/acadimics.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./acadimics.component.scss']
 })
 export class AcadimicsComponent {
-
+  acadamics=[
+  {courseType:'SSC',institute:"St Andrew's Grammer High School",passOutYear:'2016-2017',grade:'8.0',color:'#C6FC99'},
+  {courseType:'INTERMEDIATE',institute:"Sri Chaitanya Junior College",passOutYear:'2017-2019',grade:'7.25',color:'#7DE6BB'},
+  {courseType:"BACHELOR'S OF TECHNOLOGY",institute:"Netaji Institute of Engineering and Technology",passOutYear:'2019-2023',grade:'7.0',color:'#D76CFF'},
+]
 }
